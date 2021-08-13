d6170761d6bf0785cbb47140638f77d576ffde5b
EPeiseniece <87663601+EPeiseniece@users.noreply.github.com>
2021-08-13 14:35:44 +0300

    Initial Commit

---
+++ TableList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableList
+//
+//  Created by elina.peiseniece on 13/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
