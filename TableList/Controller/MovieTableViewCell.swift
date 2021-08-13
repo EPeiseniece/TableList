5aaceb4263767e2a6b0b87061d78819027c81d07
EPeiseniece <87663601+EPeiseniece@users.noreply.github.com>
2021-08-13 17:41:14 +0300

    Movies updated

---
+++ TableList/Controller/MovieTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieTableViewCell.swift
+//  TableList
+//
+//  Created by elina.peiseniece on 13/08/2021.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+ 
+    @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var movieLabel: UILabel!
+    
+}
--- TableList/Controller/MovieTableViewController.swift
+++ TableList/Controller/MovieTableViewController.swift
@@ -25,18 +25,20 @@ class MovieTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 1
     }
 
-    /*
+   
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell", for: indexPath) as? MovieTableViewCell else {return UITableViewCell()}
 
-        // Configure the cell...
+        cell.movieLabel.text = "Jurassic Park 1"
+        cell.movieImageView.image = UIImage(named: "jp.jpg")
+        
 
         return cell
     }
-    */
+   
 
     /*
     // Override to support conditional editing of the table view.
